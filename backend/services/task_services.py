ec50ab87d1f6f8a42297d0690eccc5ac37df06a0
RajTangadi03 <rtangadi03@gmail.com>
2026-02-06 11:45:12 +0530

    feat(taskService): Implement basic task services

---
+++ backend/services/task_services.py
@@ -0,0 +1,30 @@
+from dao.task_dao import TaskDao
+
+class TaskService:
+
+    @staticmethod
+    async def createTask(data):
+        return await TaskDao.create_task(
+            data.title,
+            data.description, 
+            data.assigned_user_id,
+            data.status,
+            data.due_date,
+            data.created_date
+        )
+    
+    @staticmethod
+    async def read_task_id(taskId: int):
+        # logic to check id
+
+        row = await TaskDao.read_task_id(taskId)
+        return dict(row) if row else None
+    
+    @staticmethod
+    async def read_all_task():
+        rows = await TaskDao.read_all_task()
+        return [dict(row) for row in rows]
+    
+    # update task
+
+    # add comment to task
