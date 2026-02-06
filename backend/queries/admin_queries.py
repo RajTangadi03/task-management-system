2cff35b8c2bb1e6943ec1bd7c00f0823a77d5d0b
RajTangadi03 <rtangadi03@gmail.com>
2026-02-06 12:50:18 +0530

    feat(adminQueries): Update params

--- backend/queries/admin_queries.py
+++ backend/queries/admin_queries.py
@@ -1,29 +1,29 @@
 # Admin table related CRUD operations
 
 create_admin = """
-INSERT INTO admin (name, password)
-VALUES ($1, $2)
+INSERT INTO central_admin (name, age, email, address, password, creation_time)
+VALUES ($1, $2, $3, $4, $5, NOW())
 RETURNING id;
 """
 
 read_admin = """
 SELECT *
-FROM admin
+FROM central_admin
 WHERE id = $1;
 """
 
 read_all_admin = """
 SELECT *
-FROM admin;
+FROM central_admin;
 """
 
 update_admin = """
-UPDATE admin
+UPDATE central_admin
 SET name = $1, password = $2
 WHERE id = $3;
 """
 
 delete_admin = """
-DELETE FROM admin
+DELETE FROM central_admin
 WHERE id = $1;
 """
