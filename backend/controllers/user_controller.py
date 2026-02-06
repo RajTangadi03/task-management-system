0a04df14eeed2cc2d5cd214cdd8c1d9438e5d2af
RajTangadi03 <rtangadi03@gmail.com>
2026-02-06 12:01:57 +0530

    feat(userController): Implement show and create calls

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
-from services.user_services import userServices
+from fastapi import APIRouter, HTTPException, status
+from services.user_services import UserServices
+from schemas.user_schema import userData
 
 router = APIRouter(
     tags=["Users"]
@@ -7,4 +8,18 @@ router = APIRouter(
 
 @router.get('/user/{id}')
 async def userData(id: int):
-    return await userServices.read_user_id(id)
+    return await UserServices.read_user_id(id)
+
+@router.get('/user')
+async def show_all_user():
+    return await UserServices.read_all_user()
+
+@router.post('/user-reg')
+async def create_user(data: userData):
+    try:
+        return await UserServices.create_user(data)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
