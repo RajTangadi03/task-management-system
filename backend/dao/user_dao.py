991271b8dc6ae52b3657dd6de9b3f69d13762310
RajTangadi03 <rtangadi03@gmail.com>
2026-02-06 12:20:26 +0530

    feat(userDao): Update creation, read input

--- backend/dao/user_dao.py
+++ backend/dao/user_dao.py
@@ -4,11 +4,14 @@ from database import db
 class UserDAO:
 
     @staticmethod
-    async def create_user(name, password):
+    async def create_user(name, age, email, address, password):
         async with db.pool.acquire() as conn:
             return await conn.fetchval(
                 user_qur.create_user,
                 name, 
+                age,
+                email,
+                address,
                 password
             )
         
@@ -17,5 +20,11 @@ class UserDAO:
         async with db.pool.acquire() as conn:
             row = await conn.fetchrow(user_qur.read_user_id, userId)
             return dict(row) if row else None
+        
+    @staticmethod
+    async def read_all_user():
+        async with db.pool.acquire() as conn:
+            rows = await conn.fetch(user_qur.read_all_user)
+            return [dict(row) for row in rows]
         
     
